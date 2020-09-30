cbeb7b32e14b8f4695845c26d804ef2941a3b104
Muhammad Maqsood <mmaqsood@accelaero.com>
2020-09-30 21:40:02 +0400

    Netowrk Utility added with tests

--- app/src/main/java/com/bitfinex/bitapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bitfinex/bitapp/ui/home/HomeViewModel.kt
@@ -1,12 +1,15 @@
 package com.bitfinex.bitapp.ui.home
 
+import android.util.Log
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import com.bitfinex.bitapp.data.BitAppAPI
 import com.google.gson.JsonArray
+import dagger.hilt.android.scopes.FragmentScoped
 import kotlinx.coroutines.launch
 
+@FragmentScoped
 class HomeViewModel @ViewModelInject constructor(
     private val bitAppAPI: BitAppAPI,
     @Assisted private val savedStateHandle: SavedStateHandle
@@ -17,13 +20,19 @@ class HomeViewModel @ViewModelInject constructor(
     }
     val text: LiveData<String> = _text
 
+    val  count = MutableLiveData<Int>(0)
 
     fun getPlatformStatus() = viewModelScope.launch {
+
+        count.value = count.value?.plus(1)
 
         val response : JsonArray = bitAppAPI.getTradingPairs()
         print("RESPONSE: $response")
 
+    }
 
+    override fun onCleared() {
+        super.onCleared()
     }
 
 }
--- app/src/main/java/com/bitfinex/bitapp/utils/NetworkUtils.kt
+++ app/src/main/java/com/bitfinex/bitapp/utils/NetworkUtils.kt
@@ -1,7 +1,42 @@
 package com.bitfinex.bitapp.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
 /**
  * Created by Muhammad Maqsood on 29/09/2020.
  */
 object NetworkUtils {
+
+
+    fun isNetworkAvailable(context: Context?): Boolean {
+
+        if (context == null) return false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
+    }
+
 }
